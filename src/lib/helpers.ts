7074f95f306035ad87238707ce4bd2597862450c
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2024-04-14 22:33:23 +0500

    helper function for parsing data to required format

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -6,7 +6,7 @@ export const getTodosGroupedByColumn = async () => {
     process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!
   );
 
-  const board = data.documents.reduce((acc, todo) => {
+  const columns = data.documents.reduce((acc, todo) => {
     if (!acc.get(todo.status)) {
       acc.set(todo.status, { id: todo.status, todos: [] });
     }
@@ -20,11 +20,24 @@ export const getTodosGroupedByColumn = async () => {
       description: todo?.description,
       image: todo?.image ? JSON.parse(todo.image) : null,
     });
-
     return acc;
   }, new Map<TypedColumn, Column>());
 
+  // if columns doesnt have inprogress ,done or todo add them with empty todos
+  const columnTypes: TypedColumn[] = ["todo", "inprogress", "done"];
+  columnTypes.forEach((column) => {
+    if (!columns.get(column)) {
+      columns.set(column, { id: column, todos: [] });
+    }
+  });
+
+  const sortedColumns = new Map(
+    Array.from(columns.entries()).sort(
+      (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+    )
+  );
+
   return {
-    columns: board,
+    columns: sortedColumns,
   };
 };
