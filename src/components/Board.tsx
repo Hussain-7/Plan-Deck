f9a8dd765ba7566f1005889ca7b2409f8419fd6e
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2024-04-14 21:26:14 +0500

    board skeleton added

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,9 +1,24 @@
+"use client";
 import React from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 type Props = {};
 
 const Board = (props: Props) => {
-  return <div>Board</div>;
+  // Manage the state of board here
+  return (
+    <DragDropContext
+      onDragEnd={(result) => {
+        // Handle the drag and drop event here
+      }}
+    >
+      <Droppable droppableId="board" direction="horizontal" type="colummn">
+        {(provided) => (
+          <div>{/* Render the todo, inprogress and done column here */}</div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 };
 
 export default Board;
